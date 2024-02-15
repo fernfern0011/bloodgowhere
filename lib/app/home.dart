0d84fd6e28526c3f6b5606ec94cc26f703d68a3f
Angelita Kng <70767501+AngelitaK@users.noreply.github.com>
2024-02-15 17:54:16 +0800

    shift folders

---
+++ lib/app/home.dart
@@ -0,0 +1,21 @@
+// import 'package:flutter/material.dart';
+
+// class Home extends StatefulWidget {
+//   Home({Key key, this.title}) : super(key: key);
+
+//   final String title;
+
+//   @override
+//   _HomeState createState() => _Homestate();
+// }
+
+// class _HomeState extends State<Home> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//     );
+//   }
+// }
